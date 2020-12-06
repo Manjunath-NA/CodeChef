89c1a4bbc0cf0f8eb627f66421699e7987cc3347
Manjunath Angadi <55348926+Manjunath-NA@users.noreply.github.com>
2020-12-06 11:55:57 +0530

    calculate maximum revenue by fixing the price

--- Data Structure and algorithms/DSA-4.java
+++ Data Structure and algorithms/DSA-4.java
@@ -19,6 +19,10 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		//step1: read the numbers as long
+		//step2: sort the Array
+		//step3: calculate price from lower to higher as revenue=(arr[(int)i]*(len-i));
+		//step4: store the revenue and print the maximum revenue
 		long len=in.nextInt();
 	    long arr[]=new long[(int)len];
 	    for(long i=0;i<len;i++){
